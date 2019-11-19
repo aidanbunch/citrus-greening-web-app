3d8cbcdbf810418eee2617b0236972396cc2fa2d
aidanbunch <44245721+aidanbunch@users.noreply.github.com>
2019-11-19 03:18:18 +0000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/yalmcyo8sp3p2ns/export.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/c8nqkh1shmoxxti/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['diseased_citrus_trees', 'healthy_citrus_trees']
